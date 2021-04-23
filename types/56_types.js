830df9d245657b3ce5759d7053d510ab44a9297f
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-04-23 13:20:09 -0700

    'types'

---
+++ types/56_types.js
@@ -0,0 +1,16 @@
+// Primitives
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof "To be or not to be");
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof Symbol("me"));
+// undefined is the absence of a definition.
+// null is no value
+
+// a function is simply an object in javascript;
+
+// Non-Primitives
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function () {});
---
+++ types/57_isArray.js
@@ -0,0 +1,3 @@
+// Array.isArray();
+let list = [1, 2, 3, 4, 5];
+console.log(Array.isArray(list));
