c4bd790df55e4a601dbd4a024ea1a57a41e7d8bf
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-05-08 17:58:27 -0700

    'prototypal inheritance'

---
+++ prototypal_inheritance/1.js
@@ -0,0 +1,8 @@
+// Arrays and functions are just objects in JavaScript.
+// Inheritance - geting access to the properties and methods of another object.
+
+// const array = []
+// array.__proto__
+
+// Actually no classes in JavaScript, only prototypal inheritance. 
+
